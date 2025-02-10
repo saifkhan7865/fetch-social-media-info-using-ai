8631e9902f89f465bb877b377537bb9d224d8084
Mohammed Khan <60229505+saifkhan7865@users.noreply.github.com>
2025-02-10 16:22:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,6 +91,8 @@ def get_youtube_info(channel_url):
         elif '@' in channel_url:
             # Handle new @username format
             username = channel_url.split('@')[1].split('/')[0]
+            
+            print('this is the user name that we received', username)
             # Try legacy lookup using forUsername (may fail for custom handles)
             request = youtube.channels().list(
                 part='id',
